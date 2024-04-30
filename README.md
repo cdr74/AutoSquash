d839d337e15449bfe1bab8be8e66ce2d6a206fb9
Xavier Oswald <xoswald@gmail.com>
2024-04-30 22:43:43 +0200

    Increase checks + Move SSL directly in build script

--- README.md
+++ README.md
@@ -1,62 +1,74 @@
 # AutoSquash
 
-Automate the process to download Squash TM and Orchestrator from official distribution, patch for secure and scalable operation. Create a package (tar ball or docket tbd) for distribution to infrastructure. 
+Automate the process to download Squash TM and Orchestrator from official distribution, patch for secure and scalable operation. Create a package (tar ball or docket tbd) for distribution to infrastructure.
 
-The goal is that a distribution package is created in a reproducible fashion without any manual interaction. 
+The goal is that a distribution package is created in a reproducible fashion without any manual interaction.
 
 **High level approach**
-- Use the prod tar.gz from the official distribution
-- Create a copy with all files that need changes in templates folder
-- Automate the patching and re-packaging process (coule be run in a pipeline)
+
+-   Use the prod tar.gz from the official distribution
+-   Create a copy with all files that need changes in templates folder
+-   Automate the patching and re-packaging process (coule be run in a pipeline)
 
 _Caution_: there are multiple sources of old verions on bitbucket, github etc!
-
 
 ## Monitoring
 
 Basic story for Squash TM goes like this and should be similar for the Orchestrator:
-- OTEL agent (1 per JVM) to collect Java metrics using byte code instrumentation 
-- OTEL colelctor (1 per cluster) to aggregate data from collectors and export to prometheus (format change)
-- JMX collector to get insight into Tomcat, data directly pulled from Prometheus, not involving the collector
-- Grafana to visualize data in Prometheus
 
+-   OTEL agent (1 per JVM) to collect Java metrics using byte code instrumentation
+-   OTEL colelctor (1 per cluster) to aggregate data from collectors and export to prometheus (format change)
+-   JMX collector to get insight into Tomcat, data directly pulled from Prometheus, not involving the collector
+-   Grafana to visualize data in Prometheus
 
 ## Squash TM
 
-Squash TM is a Spring Boot based app with integrated Tomcat v9. 
+Squash TM is a Spring Boot based app with integrated Tomcat v9.
 
-**Current status**
-- (done) Set proper GC param in "startup.sh"
-- (done) Enable JMX for monitoring  in "startup.sh" -> might replace this with prmetheus jmx exporter
-- (done) Disable http and enforce https in "application.properties", requires patching of war file
-- (todo) Add root CA to keystore and tune ssl for performance (out of the box is ... meh)
-- (done) Enable OTEL javaagent in "startup.sh"; needs download of the agent
-- (done) Connect with OTEL collector and the send dat to prometheus (should be seperate package for distribution)
-- (todo) Setup Grafana to visualize data in prometheus
-- (todo) Set a proper connector config for tomcat in "application.properties", requires patching of war file
-- (todo) Prod grade log config in "log4j2.xml"
-- (todo) Packaging as Docker or tar ball
+**Completed**
+
+-   (done) Set proper GC param in "startup.sh"
+-   (done) Enable JMX for monitoring in "startup.sh" -> might replace this with prmetheus jmx exporter
+-   (done) Disable http and enforce https in "application.properties", requires patching of war file
+-   (done) Enable OTEL javaagent in "startup.sh"; needs download of the agent
+-   (done) Connect with OTEL collector and the send dat to prometheus (should be seperate package for distribution)
+
+**Todo**
+
+-   (todo) Add root CA to keystore and tune ssl for performance (out of the box is ... meh)
+    -   TO BE FIXED ! Current path is encoded in war /home/chris/.keystore
+-   (todo) Setup Grafana to visualize data in prometheus
+-   (todo) Set a proper connector config for tomcat in "application.properties", requires patching of war file
+-   (todo) Prod grade log config in "log4j2.xml"
+-   (todo) Add missing plugins
+    -   SquashGitLab
+    -   BugTracker GitLab
+-   (todo) find a smarter patching approach
+    -   e.g. Set variable in the build script with the new parameters
+    -   e.g. Copy old file as .orig and replace them with the new one
+-   (todo) Packaging as Docker or tar ball
 
 All changes in config files in template folder are commented with "# CR:", could be a multi line change.
-- (todo) find a smarter patching approach
 
 We start with a pre-built tar.gz package: https://nexus.squashtest.org/nexus/#browse/browse:public-releases\
 ... alternative is start from source in: https://gitlab.com/henixdevelopment/open-source/squash\
 ... current maintenance branch is "maintenance-6.x", in case we want to compile by ourselfes\
 ... main Squash repository https://gitlab.com/henixdevelopment/open-source/squash/squashtest-tm-staging.git\
 
-SquashTM installation instructions are found here: 
+SquashTM installation instructions are found here:
 https://henixdevelopment.gitlab.io/squash/doc/squashtm-doc-en/v3/install-guide/install-squash/install-squash.html
 
-Configuring the server for certificate use (to support ssl), eg adding keystore is not covered in the script. 
-Checkout: https://tomcat.apache.org/tomcat-9.0-doc/ssl-howto.html 
+Configuring the server for certificate use (to support ssl), eg adding keystore is not covered in the script.
+Checkout: https://tomcat.apache.org/tomcat-9.0-doc/ssl-howto.html
 
 Manual steps to perform once for local tests are to setup a keystore
+
 ```
  $JAVA_HOME/bin/keytool -genkey -alias tomcat -keyalg RSA
 ```
 
 Manual steps to get OTEL collector and prometheus going
+
 ```
  docker pull otel/opentelemetry-collector
  docker  run -p 4318:4318 -v /home/chris/dev/AutoSquash/templates/SquashTM/conf/collector-config.yaml otel/opentelemetry-collector:latest &
@@ -70,7 +82,4 @@ Then open a web browser with https://localhost:9009/squash/login
 
 Prometheus runs at http://localhost:9090/
 
-
 ## Squash Orchestrator
-
-
--- squash_tm.sh
+++ squash_tm.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 set -e
 
+## COLORS
 SUCCESS="\e[32m" # GREEN COLOR
 FAIL="\e[31m"    # RED COLOR
-END="\e[0m"      # END COLOR 
+END="\e[0m"      # END COLOR
+
+## SSL CONFIGURATION
+## WARNING: Update KEY_STORE & PASSWORD before executing a build 
+SSL_PORT="server.port=9009"
+SSL_KEY_STORE="server.ssl.key-store=PATH_TO_KEYSTORE.keystore"
+SSL_KEY_STORE_PASSWORD="server.ssl.key-store-password=******"
+SSL_KEY_STORE_TYPE="server.ssl.keyStoreType=PKCS12"
+SSL_KEY_SOTRE_KEYALIAS="server.ssl.keyAlias=tomcat"
 
 BASE_DIR=$(pwd)
 
 SQUASH_RELEASE="6.0.1"
 SQUASH_REPOSITORY="https://nexus.squashtest.org/nexus/repository/public-releases/tm/core/squash-tm-distribution/"
+OPEN_TELEMETRY_AGENT="https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar"
 
 SQUASH_WORK_DIR="${BASE_DIR}/SquashTM_work"
 SQUASH_DIR="${SQUASH_WORK_DIR}/squash-tm"
@@ -19,7 +29,7 @@ SQUASH_DOWNLOAD_FULL_URL="${SQUASH_DOWNLOAD_URL}${SQUASH_DOWNLOAD_FILE}"
 
 echo "########################################################################"
 echo "#### SQUASH BUILD SCRIPT FOR ${SQUASH_DOWNLOAD_FILE}  "
-echo "#### © Copyright 2024, Christian Räss & Xavier Oswald " 
+echo "#### © Copyright 2024, Christian Räss & Xavier Oswald "
 echo "########################################################################"
 
 echo ""
@@ -28,16 +38,16 @@ echo "1. Preparing working directory [$SQUASH_WORK_DIR]"
 if [ -d "${SQUASH_WORK_DIR}" ]; then rm -Rf ${SQUASH_WORK_DIR}; fi
 mkdir ${SQUASH_WORK_DIR}
 cd ${SQUASH_WORK_DIR}
-echo  $(printf "${SUCCESS} > Success - working directory created [$SQUASH_WORK_DIR] ${END}")
+echo $(printf "${SUCCESS} > Success - working directory created [$SQUASH_WORK_DIR] ${END}")
 
 echo ""
 echo "------------------------------------------------------------------------"
 echo "2. Checking for existing remote release tarball - v${SQUASH_RELEASE}"
 echo " > Expecting ${SQUASH_DOWNLOAD_FILE}..."
-if [[ `wget -S --spider $SQUASH_DOWNLOAD_FULL_URL 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+if [[ $(wget -S --spider $SQUASH_DOWNLOAD_FULL_URL 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
   echo $(printf "${SUCCESS} > Success - Release file detected !${END}")
 else
-  echo $(printf "${FAIL} > Failure - Release file not found !${END}")
+  echo $(printf "${FAIL} > Error - Release file not found !${END}")
   exit 1
 fi
 
@@ -54,7 +64,7 @@ wget -q --show-progress -N ${SQUASH_DOWNLOAD_FULL_URL}
 if [ $? -eq 0 ]; then
   echo $(printf "${SUCCESS} > Success - Release v${SQUASH_RELEASE} has been downloaded in $(pwd)${END}")
 else
-  echo $(printf "${FAIL} > Failure - Download failed${END}")
+  echo $(printf "${FAIL} > Error - Download failed${END}")
   exit 1
 fi
 
@@ -65,18 +75,17 @@ tar -xzf ${SQUASH_DOWNLOAD_FILE}
 if [ $? -eq 0 ]; then
   echo $(printf "${SUCCESS} > Success - Squash files are extracted in $(pwd)${END}")
 else
-  echo $(printf "${FAIL} > Failure - ${SQUASH_DOWNLOAD_FILE} not found${END}")
+  echo $(printf "${FAIL} > Error - ${SQUASH_DOWNLOAD_FILE} not found${END}")
   exit 1
 fi
 rm ${SQUASH_DOWNLOAD_FILE}
 if [ $? -eq 0 ]; then
-  echo $(printf "${SUCCESS} > Success - Removed download file ${SQUASH_DOWNLOAD_FILE} from $(pwd)${END}")
+  echo $(printf "${SUCCESS} > Success - Removed downloaded file ${SQUASH_DOWNLOAD_FILE} from $(pwd)${END}")
 else
-  echo $(printf "${FAIL} > Failure - ${SQUASH_DOWNLOAD_FILE} not found${END}")
+  echo $(printf "${FAIL} > Error - ${SQUASH_DOWNLOAD_FILE} not found${END}")
   exit 1
 fi
 cd ${SQUASH_DIR}
-
 
 echo ""
 echo "------------------------------------------------------------------------"
@@ -107,34 +116,108 @@ fi
 
 echo ""
 echo "------------------------------------------------------------------------"
-echo "5. Patch config files for Java opts and SSL use"
+echo "6. Patch config files for Java opts and SSL use"
+if ! [ -x "$(command -v jar)" ]; then
+  echo $(printf "${FAIL} > Error - jar command is not available${END}")
+  exit 1
+fi
 cd ${SQUASH_WORK_DIR}
+
+## PATCHING: sartup.sh
+mv ${SQUASH_DIR}/bin/startup.sh ${SQUASH_DIR}/bin/startup.sh.orig
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Backup created ${BASE_DIR}/templates/SquashTM/bin/startup.sh.orig${END}")
+else
+  exit 1
+fi
 cp ${BASE_DIR}/templates/SquashTM/bin/startup.sh ${SQUASH_DIR}/bin/
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Updated ${BASE_DIR}/templates/SquashTM/bin/startup.sh${END}")
+else
+  exit 1
+fi
+
+## PATCHING: squash-tm.war
+
+## ADD SSL
+PROPERTY=templates/SquashTM/war/WEB-INF/classes/config/application.properties
+cp ${BASE_DIR}/${PROPERTY}.orig ${BASE_DIR}/${PROPERTY}
+echo $SSL_PORT >> ${BASE_DIR}/${PROPERTY}
+echo $SSL_KEY_STORE >> ${BASE_DIR}/${PROPERTY}
+echo $SSL_KEY_STORE_PASSWORD >> ${BASE_DIR}/${PROPERTY}
+echo $SSL_KEY_STORE_TYPE >> ${BASE_DIR}/${PROPERTY}
+echo $SSL_KEY_SOTRE_KEYALIAS >> ${BASE_DIR}/${PROPERTY}
+echo $(printf "${SUCCESS} > Success - Adding SSL support to template war file application.properties${END}")
+
 mkdir war_patch
 cd war_patch
 cp ${SQUASH_DIR}/bundles/squash-tm.war .
 jar -uf ./squash-tm.war -C ${BASE_DIR}/templates/SquashTM/war WEB-INF/classes/config/application.properties
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Creating new squash-tm.war file${END}")
+else
+  exit 1
+fi
+mv ${SQUASH_DIR}/bundles/squash-tm.war ${SQUASH_DIR}/bundles/squash-tm.war.orig
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Backup created ${SQUASH_DIR}/bundles/squash-tm.war.orig${END}")
+else
+  exit 1
+fi
 cp ./squash-tm.war ${SQUASH_DIR}/bundles/squash-tm.war
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Updated ${SQUASH_DIR}/bundles/squash-tm.war${END}")
+else
+  exit 1
+fi
+
 cd ${SQUASH_WORK_DIR}
 rm -rf war_patch
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Clean-up${END}")
+else
+  exit 1
+fi
 
 ## XAVIER
-## TODO: Download from GitHub might be restricted 
+## TODO: Download from GitHub might be restricted
 ## Is Open-telemetry package available in our Nexus setup ?
 
 echo ""
 echo "------------------------------------------------------------------------"
-echo "Enable monitoring with OTEL agent (if this works, let's close JMX port)"
+echo "7. Enable monitoring with OTEL agent (if this works, let's close JMX port)"
 cd ${SQUASH_WORK_DIR}
-wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
-mv opentelemetry-javaagent.jar ${SQUASH_DIR}/bundles
 
+if [[ $(wget -S --spider $OPEN_TELEMETRY_AGENT 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
+  echo $(printf "${SUCCESS} > Success - Telemetry agent file detected !${END}")
+else
+  echo $(printf "${FAIL} > Error - Telemetry agent file not found !${END}")
+  exit 1
+fi
+
+wget -q --show-progress -N ${OPEN_TELEMETRY_AGENT}
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Telemetry agent has been downloaded !${END}")
+else
+  echo $(printf "${FAIL} > Error - Download failed!${END}")
+  exit 1
+fi
+
+mv opentelemetry-javaagent.jar ${SQUASH_DIR}/bundles
+if [ $? -eq 0 ]; then
+  echo $(printf "${SUCCESS} > Success - Telemetry agent set in ${SQUASH_DIR}/bundles${END}")
+fi
+
+echo ""
+echo $(printf "${SUCCESS}The setup is Ready !${END}")
+echo "Go to ${SQUASH_DIR}/bin and run startup.sh for tests"
+exit 0
+
+# wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
 # docker pull otel/opentelemetry-collector
 # docker  run -p 4318:4318 -v /home/chris/dev/AutoSquash/templates/SquashTM/conf/collector-config.yaml otel/opentelemetry-collector:latest &
 # docker pull prom/prometheus
 # docker run -p 9090:9090 -v /home/chris/dev/AutoSquash/templates/SquashTM/conf/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus &
-
-echo "Go to ${SQUASH_DIR}/bin and run startup.sh for tests"
 
 echo ""
 echo "------------------------------------------------------------------------"
